038a9974d485cfab711a8213fad30fc944e3c941
Sun5hine <af5kh@yahoo.com>
2019-02-28 19:25:38 +0000

    Update form.js

--- src/form.js
+++ src/form.js
@@ -4,9 +4,7 @@ import React from 'react';
 class Form extends React.Component {
   constructor(props){
     super(props);
-    this.state = {
-      value: ''
-    }
+    
 
     // this.handleChange = this.handleChange.bind(this);
     // this.handleSubmit = this.handleSubmit.bind(this);
